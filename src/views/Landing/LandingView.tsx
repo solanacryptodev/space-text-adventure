62c429c06fde4e7651dd043a28d93aa82d1946c7
solanacryptodev <eoscrypto87@protonmail.com>
2023-08-01 20:29:52 -0400

    feat - integrated flowbite. Added LandingView framing and tested website color themes. WIP.

--- src/views/Landing/LandingView.tsx
+++ src/views/Landing/LandingView.tsx
@@ -1,47 +1,27 @@
 import React, { FC } from 'react';
-import Link from 'next/link';
-import { testEndpoint } from '~/lib/mercury';
 import { observer } from 'mobx-react-lite';
 import { ProfileView } from '~/views/Profile/ProfileView';
+import { LandingHeaderView } from '~/views/Landing/LandingHeader/LandingHeaderView';
+import { LandingSidebarView } from '~/views/Landing/LandingSidebar/Sidebar';
+import { LandingMain } from '~/views/Landing/LandingMain/LandingMain';
+import Config from 'tailwind.config';
 
 export const LandingView: FC = observer(() => {
-  const hello = testEndpoint();
+  const { theme } = Config;
+  const themeGradiantStart = theme.extend.colors.primary['200'];
+  const themeGradiantEnd = theme.extend.colors.primary['400'];
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
-      <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-        <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-          Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
-        </h1>
-        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
-          <Link
-            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-            href="https://create.t3.gg/en/usage/first-steps"
-            target="_blank"
-          >
-            <h3 className="text-2xl font-bold">First Steps →</h3>
-            <div className="text-lg">
-              Just the basics - Everything you need to know to set up your database and
-              authentication.
-            </div>
-          </Link>
-          <Link
-            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-            href="https://create.t3.gg/en/introduction"
-            target="_blank"
-          >
-            <h3 className="text-2xl font-bold">Documentation →</h3>
-            <div className="text-lg">
-              Learn more about Create T3 App, the libraries it uses, and how to deploy it.
-            </div>
-          </Link>
-        </div>
-        <div className="flex flex-col items-center gap-2">
-          <p className="text-2xl text-white">{hello || 'Loading tRPC query...'}</p>
-          <ProfileView />
-        </div>
-      </div>
-    </main>
+    <div className="antialiased bg-gradient-to-b from-[#1f2933] to-[#151B25] dark:bg-gray-900">
+      {/* Header */}
+      <LandingHeaderView />
+
+      {/* Sidebar */}
+      <LandingSidebarView />
+
+      {/* Main Section */}
+      <LandingMain />
+    </div>
   );
 });
 
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,45 @@
 import { Config } from 'tailwindcss';
+import * as colors from 'tailwindcss/colors';
 
 export default {
-  content: ['./src/**/*.{js,ts,jsx,tsx}'],
+  darkMode: 'class',
+  content: [
+    './src/**/*.{js,ts,jsx,tsx}',
+    'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}',
+    'node_modules/flowbite-react/**/*.js',
+    './src/pages/**/*.{ts,tsx}',
+    './public/**/*.html',
+  ],
   theme: {
-    extend: {},
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    colors: {
+      gray: colors.gray,
+      blue: colors.sky,
+      red: colors.rose,
+      pink: colors.fuchsia,
+    },
+    extend: {
+      colors: {
+        primary: {
+          '50': '#eff6ff',
+          '100': '#dbeafe',
+          '200': '#bfdbfe',
+          '300': '#93c5fd',
+          '400': '#60a5fa',
+          '500': '#3b82f6',
+          '600': '#2563eb',
+          '700': '#1d4ed8',
+          '800': '#1e40af',
+          '900': '#1e3a8a',
+          '950': '#172554',
+        },
+      },
+    },
   },
-  plugins: [],
+  plugins: [require('flowbite/plugin')],
 } satisfies Config;
